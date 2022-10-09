7b9596453187e16640f6c6f10e90b5b5dd7c8404
Danish <danishzulfiqar100@gmail.com>
2022-10-09 13:29:09 +0500

    added a new

---
+++ Credit Score/code.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int x;
+    cin>>x;
+
+    if(x>=750)
+    cout<<"YES"<<endl;
+
+    else
+    cout<<"No"<<endl;
+
+    return 0;
+}
