af363d6a5b3f9bf59d2a6605c09b1e305c78bb86
Danish <danishzulfiqar100@gmail.com>
2022-10-12 23:23:54 +0500

    git

---
+++ Increase Iq/code.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    n = n - 7;
+
+    if (n > 170)
+        cout << "YES" << endl;
+
+    else
+        cout << "NO" << endl;
+        
+    return 0;
+}
